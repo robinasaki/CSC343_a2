1a09bb3e4e25b34ea8d511a99e8f8a7e38f4a832
Lin Gordon <lingord2@teach.cs.toronto.edu>
2024-03-06 17:20:54 -0500

    Add comments for views

--- q6.sql
+++ q6.sql
@@ -9,40 +9,37 @@ CREATE TABLE q6 (
     devotedness INT NOT NULL
 );
 
--- Do this for each of the views that define your intermediate steps.
--- (But give them better names!) The IF EXISTS avoids generating an error
--- the first time this file is imported.
--- If you do not define any views, you can delete the lines about views.
 DROP VIEW IF EXISTS books_with_one_author CASCADE;
 DROP VIEW IF EXISTS authors_with_two_plus_books CASCADE;
 DROP VIEW IF EXISTS author_books CASCADE;
 DROP VIEW IF EXISTS satisfactory_checkouts CASCADE;
 DROP VIEW IF EXISTS specific_checkouts CASCADE;
 DROP VIEW IF EXISTS satisfactory_reviews CASCADE;
-
 DROP VIEW IF EXISTS no_devotedness CASCADE;
 DROP VIEW IF EXISTS has_devotedness CASCADE;
 DROP VIEW IF EXISTS final_result CASCADE;
 
--- Define views for your intermediate steps here:
+-- All books that have only one author
 CREATE VIEW books_with_one_author AS
     SELECT max(HoldingContributor.contributor) AS author, HoldingContributor.holding AS book
     FROM HoldingContributor
     GROUP BY HoldingContributor.holding
     HAVING count(HoldingContributor.contributor) = 1;
 
+-- All authors who have 2+ books in the system, as well as the count of how many
 CREATE VIEW authors_with_two_plus_books AS
     SELECT author, count(book) AS numBooks
     FROM books_with_one_author
     GROUP BY author
     HAVING count(book) >= 2;
 
+-- Specific ids of all books written by above authors
 CREATE VIEW author_books AS
     SELECT authors_with_two_plus_books.author, HoldingContributor.holding
     FROM authors_with_two_plus_books JOIN HoldingContributor 
         ON HoldingContributor.contributor = authors_with_two_plus_books.author;
 
-
+-- All patrons who have checked out almost all (n - 1) of the above authors' books
 CREATE VIEW satisfactory_checkouts AS
     SELECT Checkout.patron, author_books.author, count(author_books.holding) AS checkoutCount
     FROM Checkout JOIN LibraryHolding ON Checkout.copy = LibraryHolding.barcode
@@ -50,18 +47,15 @@ CREATE VIEW satisfactory_checkouts AS
     GROUP BY Checkout.patron, author_books.author
     HAVING count(author_books.holding) >= (SELECT numBooks FROM authors_with_two_plus_books 
                                             WHERE author_books.author = authors_with_two_plus_books.author) - 1;
-    
+
+-- The specific book checkout details of the above patrons
 CREATE VIEW specific_checkouts AS
     SELECT Checkout.patron, author_books.author, author_books.holding
     FROM Checkout JOIN LibraryHolding ON Checkout.copy = LibraryHolding.barcode
         JOIN author_books ON LibraryHolding.holding = author_books.holding
     WHERE Checkout.patron IN (SELECT patron FROM satisfactory_checkouts);
-    -- SELECT Checkout.patron, authors_with_two_plus_books.author, count(authors_with_two_plus_books.author)
-    -- FROM Checkout JOIN LibraryHolding ON Checkout.copy = LibraryHolding.barcode
-    --     JOIN HoldingContributor ON HoldingContributor.holding = LibraryHolding.holding
-    -- WHERE HoldingContributor.contributor IN (SELECT author FROM authors_with_two_plus_books)
-    -- GROUP BY Checkout.patron, authors_with_two_plus_books.author;
-    
+
+-- Patrons who have met the review requirements (reviewed all checkouts of author, plus avg review is >= 4.0 stars)
 CREATE VIEW satisfactory_reviews AS
     SELECT Review.patron, specific_checkouts.author, count(specific_checkouts.holding) AS numReviews, avg(Review.stars) AS avgReview
     FROM Review JOIN specific_checkouts ON (Review.patron = specific_checkouts.patron 
@@ -72,10 +66,12 @@ CREATE VIEW satisfactory_reviews AS
                                                     AND specific_checkouts.author = satisfactory_checkouts.author)
         AND avg(Review.stars) >= 4.0;
 
+-- A list of all patrons with 0 set as a temporary devotedness.
 CREATE VIEW no_devotedness AS
     SELECT card_number AS patron, 0 AS devotedness
     FROM Patron;
 
+-- All patrons with any devotedness towards any author.
 CREATE VIEW has_devotedness AS
     SELECT satisfactory_checkouts.patron AS patron, count(satisfactory_checkouts.author) AS devotedness
     FROM satisfactory_checkouts
@@ -84,6 +80,7 @@ CREATE VIEW has_devotedness AS
     AND satisfactory_checkouts.author = satisfactory_reviews.author
     GROUP BY satisfactory_checkouts.patron;
 
+-- All patrons with the number of authors they are devoted to
 CREATE VIEW final_result AS
     SELECT no_devotedness.patron AS patronID, coalesce(has_devotedness.devotedness, no_devotedness.devotedness) AS devotedness
     FROM no_devotedness
